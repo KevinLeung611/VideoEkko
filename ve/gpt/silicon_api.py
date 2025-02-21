bb4949b71f1f0c884a847782fc65068b0f36c57f
kevinleung <ocean_talk@outlook.com>
2025-02-21 23:47:39 +0800

    feat: add page load logic

--- ve/gpt/silicon_api.py
+++ ve/gpt/silicon_api.py
@@ -7,29 +7,29 @@ import requests
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from ve.common import config
 
-config = config.get_config('gpt')
-
 _BASE_URL = "https://api.siliconflow.cn"
-_API_KEY = config['apiKey']
-_MODEL = config['model']
 
-if not _API_KEY:
-    raise SystemExit("API key is not set! Check the conf.yaml file and add api key")
-
-if not _MODEL:
-    raise SystemExit("Model is not set! Check the conf.yaml file and add model")
 
 def completions(system_prompt: str, user_prompt: str):
+    api_key = config.get_config('gpt')['apiKey']
+    model = config.get_config('gpt')['model']
+
+    if not api_key:
+        raise SystemExit("API key is not set! Check the conf.yaml file and add api key")
+
+    if not model:
+        raise SystemExit("Model is not set! Check the conf.yaml file and add model")
+
     api_path = "/v1/chat/completions"
     completions_url = parse.urljoin(_BASE_URL, api_path)
 
     headers = {
-        "Authorization": f"Bearer {_API_KEY}",
+        "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json"
     }
 
     payload = {
-        "model": _MODEL,
+        "model": model,
         "messages": [
             {
                 "role": "system",
--- web.py
+++ web.py
@@ -3,6 +3,8 @@ import gradio as gr
 import engine as ve_engine
 from ve.common import config
 
+_DEFAULT_PLATFORM = 'silicon'
+
 _PLATFOM_MODEL_MAP = {
     "silicon": ["Qwen/Qwen2.5-72B-Instruct", "Pro/deepseek-ai/DeepSeek-R1"],
     "DeepSeek": ["R1", "V3"]
@@ -16,27 +18,39 @@ _LANG_MAP = {
 }
 
 
+def page_load():
+    whisper_config = config.get_config('whisper')
+    if whisper_config:
+        whisper_model = 'turbo' if not whisper_config['model'] else whisper_config['model']
+        video_lang = 'English' if not whisper_config['language'] else whisper_config['language']
+
+    gpt_config = config.get_config('gpt')
+    if gpt_config:
+        platform = _DEFAULT_PLATFORM if not gpt_config['platform'] else gpt_config['platform']
+        api_key = gpt_config['apiKey']
+        gpt_model = _PLATFOM_MODEL_MAP.get(_DEFAULT_PLATFORM)[0] if not gpt_config['model'] else gpt_config['model']
+
+    reverse_lang_map = {}
+    for k, v in _LANG_MAP.items():
+        reverse_lang_map[v] = k
+
+    return [platform, gpt_model, api_key, whisper_model, reverse_lang_map[video_lang], '中文']
+
+
 def gpt_platform_change(value):
-    config.get_config('gpt')['platform'] = value
     return gr.Dropdown(choices=_PLATFOM_MODEL_MAP[value], value=_PLATFOM_MODEL_MAP[value][0])
 
-def gpt_model_change(value):
-    config.get_config('gpt')['model'] = value
-
-def api_key_change(value):
-    config.get_config('gpt')['api_key'] = value
-
-def whisper_model_change(value):
-    config.get_config('whisper')['model'] = value
-
-def video_lang_change(value):
-    config.get_config('whisper')['language'] = _LANG_MAP[value]
-
-def translate_lang_change(value):
-    pass
-
-def generate_video(video_path):
+def generate_video(gpt_platform, gpt_model, api_key, whisper_model, video_lang, translate_lang, video_path):
     try:
+        if not api_key:
+            raise gr.Error("It's require an api key. please check.")
+
+        config.get_config('gpt')['platform'] = gpt_platform
+        config.get_config('gpt')['model'] = gpt_model
+        config.get_config('gpt')['apiKey'] = api_key
+        config.get_config('whisper')['model'] = whisper_model
+        config.get_config('whisper')['language'] = _LANG_MAP[video_lang]
+
         video_paths = ve_engine.generate_videos(video_path)
         if len(video_paths) == 0:
             raise RuntimeError("No video generated")
@@ -48,6 +62,7 @@ def generate_video(video_path):
         print(e)
         raise gr.Error("生成视频失败")
 
+
 def show_interface(demo: gr.Blocks):
     with gr.Sidebar():
         gr.Markdown(
@@ -58,21 +73,14 @@ def show_interface(demo: gr.Blocks):
             """
         )
         with gr.Row():
-            platform_default = 'silicon'
-            gpt_platform = gr.Dropdown(_PLATFOM_MODEL_MAP.keys(), label="GPT平台", value=platform_default)
-            gpt_model = gr.Dropdown(_PLATFOM_MODEL_MAP[platform_default], label="GPT模型", value=_PLATFOM_MODEL_MAP[platform_default][0])
-            api_key = gr.Textbox(label="API_KEY", placeholder="请输入对应的API_KEY")
-            whisper_model = gr.Dropdown(_WHISPER_MODEL, label="翻译模型", value="turbo")
-            video_lang = gr.Dropdown(list(_LANG_MAP.keys()), label="原视频语言", value="英文")
-            translate_lang = gr.Dropdown(list(_LANG_MAP.keys()), label="生成视频语言", value="中文")
+            gpt_platform = gr.Dropdown(_PLATFOM_MODEL_MAP.keys(), label="GPT平台")
+            gpt_model = gr.Dropdown(_PLATFOM_MODEL_MAP[_DEFAULT_PLATFORM], label="GPT模型")
+            api_key = gr.Text(label="API_KEY", placeholder="请输入对应的API_KEY", lines=2)
+            whisper_model = gr.Dropdown(_WHISPER_MODEL, label="翻译模型")
+            video_lang = gr.Dropdown(list(_LANG_MAP.keys()), label="原视频语言")
+            translate_lang = gr.Dropdown(list(_LANG_MAP.keys()), label="生成视频语言")
 
             gpt_platform.change(gpt_platform_change, inputs=gpt_platform, outputs=gpt_model)
-            gpt_model.change(gpt_model_change, inputs=gpt_model)
-            api_key.blur(api_key_change, inputs=api_key)
-            whisper_model.change(whisper_model_change, inputs=whisper_model)
-            video_lang.change(video_lang_change, inputs=video_lang)
-            translate_lang.change(translate_lang_change, inputs=translate_lang)
-
     with gr.Row():
         with gr.Column(scale=1):
             pass
@@ -81,9 +89,14 @@ def show_interface(demo: gr.Blocks):
             trans_btn = gr.Button("翻译", variant="primary")
             output_video = gr.PlayableVideo(label="生成视频")
 
-            trans_btn.click(generate_video, inputs=input_video, outputs=output_video)
+            trans_btn.click(generate_video,
+                            inputs=[gpt_platform, gpt_model, api_key, whisper_model, video_lang, translate_lang, input_video],
+                            outputs=output_video)
         with gr.Column(scale=1):
             pass
+
+    # Page loaded config
+    demo.load(fn=page_load, outputs=[gpt_platform, gpt_model, api_key, whisper_model, video_lang, translate_lang])
 
 
 with gr.Blocks(theme=gr.themes.Soft()) as demo:
